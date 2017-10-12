bef4154e28c2761b97c8e6c2a1578e2514441982
velizjuan96 <juancarlosveliz96@hotmail.com>
2017-10-11 20:22:06 -0600

    Add files via upload

---
+++ programacion orientada a objetos 6 examen/src/sample/Controller.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class Controller {
+
+    public TextField letras;
+
+
+    public void enviar(ActionEvent actionEvent) {
+
+
+
+
+
+
+    }
+}
